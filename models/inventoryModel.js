9b8c39649a5d0e90e56b809b4ea4cb9619a71b72
VinayJangotra <109384165+VinayJangotra@users.noreply.github.com>
2024-08-26 15:39:45 +0530

    Inventory Schema

---
+++ models/inventoryModel.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const inventorySchema = new mongoose.Schema({
+    inventoryType:{
+        type: String,
+        required: [true, 'inventory type require'],
+        enum:['in','out']
+    },
+    bloodGroup:{
+        type: String,
+        required: [true, 'blood group require'],
+        enum:['A+','A-','B+','B-','AB+','AB-','O+','O-']
+    },
+    
+    quantity:{
+        type: Number,
+        required: [true, 'quantity require'],
+    },
+    organisation:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+        required: [true, 'organisation require'],
+    },
+    hospital:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+        required: function(){
+            if(this.inventoryType === 'out'){
+                return true;
+            }
+            return false;
+        }
+    },
+    donar:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+        required: function(){
+            if(this.inventoryType === 'in'){
+                return true;
+            }
+            return false;
+        }
+    }, 
+},{timestamps:true});
